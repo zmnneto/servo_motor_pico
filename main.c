885d262ad0b61dba88c08e70173984a49ada7d34
Leo <leozamboneneto123@gmail.com>
2026-03-03 13:52:01 -0300

    remova void i2c

--- main.c
+++ main.c
@@ -1,41 +1,63 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
 #include "hardware/pwm.h"
+#include "hardware/i2c.h"
 #include "define.h"
 
+//kkkkk vi um nerdola fazendo em .go, find about it!
+void i2c_init_display(){
+  i2c_init(I2C_MODULE, DISPLAY_FREQ);
+  gpio_set_function(,GPIO_SET_I2C)
+}
 void servo_init(uint gpio)
 {
   gpio_set_function(gpio, GPIO_FUNC_PWM);
-  fatia_pwm = pwm_gpio_to_slice_num(gpio);
+  slice_pwm = pwm_gpio_to_slice_num(gpio);
 
   pwm_config cfg = pwm_get_default_config();
   pwm_config_set_clkdiv(&cfg, CLOCK_DIV);
 
   pwm_wrap = (CPU_CLOCK / (CLOCK_DIV * PWM_FREQ)) - 1;
   pwm_config_set_wrap(&cfg, pwm_wrap);
-  pwm_init(fatia_pwm, &cfg, true);
+  pwm_init(slice_pwm, &cfg, true);
 }
 
-void servo_angulo_pwm(float angle)
+void servo_angle_pwm(float angle)
 {
   if (angle < SERVO_MIN_ANGLE) angle = SERVO_MIN_ANGLE;
-  else if (angle > SERVO_MAX_ANGLE) angle = SERVO_MAX_ANGLE;
+  if (angle > SERVO_MAX_ANGLE) angle = SERVO_MAX_ANGLE;
 
-uint16_t pulso_us = SERVO_MIN_US + (angle / SERVO_MAX_ANGLE)* (SERVO_MAX_US - SERVO_MIN_US);
+  uint16_t pulso_us = SERVO_MIN_US + (angle / SERVO_MAX_ANGLE)* (SERVO_MAX_US - SERVO_MIN_US);
 
   uint32_t nivel = (pulso_us * pwm_wrap) / PWM_PERIODO_US;
-  pwm_set_gpio_level(PINO_SERVO, nivel);
+  
+  pwm_set_gpio_level(GPIO_SERVO , nivel);
+  
 }
 
 
 int main() {
   stdio_init_all();
-  while (true) {
-servo_init(PINO_SERVO);
-  for (int i = SERVO_MIN_ANGLE; i <= SERVO_MAX_ANGLE; i++){
-    sleep_ms(30);
-    servo_angulo_pwm(i);
-    
+
+  servo_init(GPIO_SERVO);
+  
+  gpio_init(push_btn);
+  gpio_set_dir(push_btn, GPIO_IN);
+  gpio_pull_up(push_btn);
+
+  servo_angle_pwm(angle);
+  
+  while(true){
+
+    if(!gpio_get(push_btn))
+    {
+        angle += 10;
+          if(angle > SERVO_MAX_ANGLE) angle = SERVO_MIN_ANGLE;
+            servo_angle_pwm(angle);
+            sleep_ms(300);  
+              
+    }
   }
-  }
+
+  return 0;
 }
